3f4a7e59bd1f5f3e838d8dee34d2473931f6b455
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-07-20 20:49:00 +0200

    Create reload.js

---
+++ reload.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, client, message) => {	
+  if(message.author.id !== "252873409401323520") return message.reply("Tu n\'es pas l\'owner, tu ne peux pas reload le bot !");
+                message.react('?')
+                    .then(message => client.destroy())
+                    .then(() => client.login(process.env.BOT_TOKEN));
+}
+
+module.exports.help = {
+  name:"reload"
+}
