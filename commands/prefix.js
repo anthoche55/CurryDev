3c9cf2c1420cf97ae0bd755feffbd7f85d02dbed
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-07-20 22:07:09 +0200

    Create prefix.js

---
+++ commands/prefix.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+module.exports.run = async (bot, message, args) => {
+
+  if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("Non, tu n\'es pas autorisé à faire cette commande !!!");
+  if(!args[0] || args[0 == "help"]) return message.reply("Utilisation: !prefix <préfix>");
+
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+
+  prefixes[message.guild.id] = {
+    prefixes: args[0]
+  };
+
+  fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
+    if (err) console.log(err)
+  });
+
+  let sEmbed = new Discord.RichEmbed()
+  .setColor("#FF9900")
+  .setTitle("Prefix mis !")
+  .setDescription(`Prefix: ${args[0]}`);
+
+  message.channel.send(sEmbed);
+
+}
+
+module.exports.help = {
+  name: "prefix"
+}
