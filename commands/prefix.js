4954dfc85f2b590e704f89e40b5aad6536acea36
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-07-26 23:16:20 +0200

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -15,7 +15,7 @@ module.exports = {
     prefixes: args[0]
   };
 
-  fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
+  fs.writeFile("../prefixes.json", JSON.stringify(prefixes), (err) => {
     if (err) console.log(err)
   });
 
