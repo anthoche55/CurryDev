72fae05ef37dc578e4faefc3dc3445b909ced071
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-07-26 22:46:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,79 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const prefix = "&"
+const prefix = "&";
 
-const bot = new Discord.Client();
-bot.commands = new Discord.Collection();
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	bot.commands.set(command.name, command);
+	client.commands.set(command.name, command);
 }
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} est en ligne sur ${bot.guilds.size} serveurs!`);
+const cooldowns = new Discord.Collection();
+
+client.on('ready', () => {
+	console.log('Prêt!');
+});
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+	if (!command) return;
+
+	if (command.guildOnly && message.channel.type !== 'text') {
+		return message.reply('Je ne peux pas executer cette commande en MP!');
+	}
+
+	if (command.args && !args.length) {
+		let reply = `Tu n\'as mis aucun argument, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nL\'utilisation est: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (!timestamps.has(message.author.id)) {
+		timestamps.set(message.author.id, now);
+		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+	}
+	else {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`Attend ${timeLeft.toFixed(1)} seconde(s) avant d\'executer la commande \`${command.name}\`.`);
+		}
+
+		timestamps.set(message.author.id, now);
+		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+	}
+
+	try {
+		command.execute(message, args);
+	}
+	catch (error) {
+		console.error(error);
+		message.reply('Il y a eu une erreure avec l\'execution de la commande.');
+	}
 });
 
 bot.login(process.env.BOT_TOKEN);
