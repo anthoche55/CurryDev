54bf4cbb2ba8071063254d14fd4056ea50fa9cb2
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-07-26 22:19:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,21 @@
-const Discord = require("discord.js");
-const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true});
-bot.commands = new Discord.Collection();
-const prefix = "&";
-const {area, perimeter} = require('npm-helloworld-testttt')
+const fs = require('fs');
+const Discord = require('discord.js');
+const prefix = "&"
 
-fs.readdir("./commands/", (err, files) => {
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js");
-  if(jsfile.length <= 0){
-    console.log("Je ne sais pas trouver les commandes");
-    return;
-  }
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-  jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-  });
-});
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} est en ligne sur ${bot.guilds.size} serveurs!`);
   console.log(area(1))
   console.log(perimeter(0.5))
-});
-
-
-bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
-
 });
 
 bot.login(process.env.BOT_TOKEN);
