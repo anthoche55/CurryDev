0d7f4a575f656cac683d355b5fedee3ee217d99f
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-07-26 22:22:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const Discord = require('discord.js');
 const prefix = "&"
 
 const bot = new Discord.Client();
-client.commands = new Discord.Collection();
+bot.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+	.commands.set(command.name, command);
 }
 
 bot.on("ready", async () => {
