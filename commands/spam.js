20d3c3f1885e2c8cea05691e9ee7337dd741eca4
Cecemelpvp <cedricthauvoye1@gmail.com>
2018-07-27 20:21:28 +0200

    Create spam.js

---
+++ commands/spam.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+module.exports = {
+	name: 'spam',
+	description: '&spam',
+	execute(message, args) {
+  
+message.channel.send(args)
+message.channel.send(args)
+message.channel.send(args)
+message.channel.send(args)
+message.channel.send(args)
+message.channel.send(args)
+message.channel.send(args)
+  
+	},
+};
